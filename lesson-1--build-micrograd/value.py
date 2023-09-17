3433cd6b49e6bb28825744c4a9af04fa2df38e1e
Daniel Waltrip <dwaltrip77@gmail.com>
2023-09-17 10:35:01 -0700

    Lesson 1 - Simple visualization of the computation graph

--- lesson-1--build-micrograd/value.py
+++ lesson-1--build-micrograd/value.py
@@ -1,8 +1,13 @@
+from graphviz import Digraph
+
 
 class Value:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
+        self.label = label
+
         self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
         return f"Value({self.data})"
@@ -17,3 +22,34 @@ class Value:
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
         return out
+
+
+def _draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+
+    nodes, edges = _trace_graph(root)
+    for n in nodes:
+        uid = str(id(n))
+        label = '{' + f'{n.label} | {n.data:.4f}' + '}'
+        dot.node(uid, label=label, shape='record')
+
+        if n._op:
+            dot.node(name = uid + n._op, label=n._op)
+            dot.edge(uid + n._op, uid)
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
+
+def _trace_graph(root):
+  """ builds a set of all nodes and edges in a graph """
+  nodes, edges = set(), set()
+  def build(v):
+    if v not in nodes:
+      nodes.add(v)
+      for child in v._prev:
+        edges.add((child, v))
+        build(child)
+  build(root)
+  return nodes, edges
