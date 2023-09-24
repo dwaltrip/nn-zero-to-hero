fb666fed243e4ea8a2b326f6bd1bc82c11d13341
Daniel Waltrip <dwaltrip77@gmail.com>
2023-09-24 15:06:26 -0700

    Rename "lesson 1" to "lecture 1"

--- tools/launch-repl.sh
+++ tools/launch-repl.sh
@@ -3,8 +3,7 @@
 TOOLS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$TOOLS_DIR")"
 
-LESSON_1_DIR="$ROOT_DIR/lesson-1--build-micrograd"
-
+LESSON_1_DIR="$ROOT_DIR/lecture-1--build-micrograd"
 export PYTHONPATH="$LESSON_1_DIR:$PYTHONPATH"
 
 source "$ROOT_DIR/venv/bin/activate"
